4d7700bb8bf9ae3a72f6a13fb9ae8769c98d63e6
a.ignatova24 <a.ignatova24@LAB108PC09>
2024-02-12 13:46:41 +0200

    Add project files.

---
+++ currency/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Security.Authentication;
+namespace Classes;
+public class Converter
+{
+    public double currency;
+    public Boolean currtype; // false > bgtoeur | true > eurtobg
+    public double convcurr = Converter.Exchange();
+    public Converter () { }
+    public Converter (double curruncy, Boolean currtype) { this.currency = currency; this.currtype = currtype; }
+    public double Exchange ()
+    {
+        double coeff = 0.51;
+        if(currtype == false) { return coeff * currency; } else { return currency / coeff; }
+    }
+}
